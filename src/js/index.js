c019863c1b785831e5406b3607c14dc27d65d56c
oscar1630 <osmolomax@gmail.com>
2024-07-02 11:49:42 +0200

    dads

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,14 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+// - Usando cualquiera de los tres HTML que te doy, haz que al hacer click en el botón se vayan añadiendo items con el texto Item 1, Item 2, Item 3,... Los item deben llevar el número correspondiente independiente de qué HTML hayas elegido
+
+const buttonElement = document.getElementById('boton')
+
+const clickButton = ()=>{
+    if(buttonElement.addEventListener('click')){
+        const newText = document.createElement('h1')
+        newText.textContent='Item 2'
+    }
+    console.log(clickButton)
+}
